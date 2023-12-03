d07c7d7db13225592eba124b76a8aa25c793368f
Duffylo <ma.kost@abv.bg>
2023-12-03 03:38:21 +0200

    added client

---
+++ ClientConsoleApp/ClientConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlcatrazGrpcService;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new TimeService.TimeServiceClient(channel);
+var reply = await client.GetTimeAsync(new Empty());
+Console.WriteLine("Time: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
